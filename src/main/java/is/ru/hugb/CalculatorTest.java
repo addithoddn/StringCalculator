225a26d0563372561c2d7e7111b5db40460aceb8
Arnar Þórðarson <arnart08@ru.is>
2017-10-03 23:22:42 +0000

    set up folder structur and build scripts

---
+++ src/main/java/is/ru/hugb/CalculatorTest.java
@@ -0,0 +1,8 @@
+package is.ru.hugb;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class CalculatorTest
+{
+
+}
---
+++ src/main/java/is/ru/hugb/StringCalculator.java
@@ -0,0 +1,12 @@
+package is.ru.hugb;
+
+public class StringCalculator
+{
+	public static void main(String[] args)
+	{
+	}
+	
+	public static int add(String text)
+	{
+	}
+}
